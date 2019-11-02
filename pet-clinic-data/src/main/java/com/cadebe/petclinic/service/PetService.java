851295944cbd54edcf373ccca9ced83199b88261
Carla <carla.debeer.uk@gmail.com>
2019-11-02 15:09:59 +0100

    Implement interfaces for services. Closes #1.

---
+++ pet-clinic-data/src/main/java/com/cadebe/petclinic/service/PetService.java
@@ -0,0 +1,14 @@
+package com.cadebe.petclinic.service;
+
+import com.cadebe.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/cadebe/petclinic/service/VetService.java
@@ -0,0 +1,15 @@
+package com.cadebe.petclinic.service;
+
+import com.cadebe.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
+
