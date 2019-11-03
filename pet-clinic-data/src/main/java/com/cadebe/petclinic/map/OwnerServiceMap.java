fa66eff12934a072ffc16d2154c9adac5b270353
Carla <carla.debeer.uk@gmail.com>
2019-11-03 11:08:07 +0100

    Implement map-based services. Closes #3.

---
+++ pet-clinic-data/src/main/java/com/cadebe/petclinic/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package com.cadebe.petclinic.map;
+
+import com.cadebe.petclinic.model.Owner;
+import com.cadebe.petclinic.service.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/cadebe/petclinic/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package com.cadebe.petclinic.map;
+
+import com.cadebe.petclinic.model.Pet;
+import com.cadebe.petclinic.service.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
