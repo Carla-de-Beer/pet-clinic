82ad28242d3e19e63a2951548b6c55349cd575f4
Carla <carla.debeer.uk@gmail.com>
2019-11-03 20:39:58 +0100

    Create PetType Map Service Impl. Closes #24.

---
+++ pet-clinic-data/src/main/java/com/cadebe/petclinic/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package com.cadebe.petclinic.map;
+
+import com.cadebe.petclinic.model.PetType;
+import com.cadebe.petclinic.service.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/cadebe/petclinic/service/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.cadebe.petclinic.service;
+
+import com.cadebe.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>  {
+}
