f6daf4f7a1602ab7beff39b1c15de5f1085e8ba0
Carla <carla.debeer.uk@gmail.com>
2019-11-03 20:45:00 +0100

    Add PetTypes in with Bootstrap (startup). Closes #27.

--- pet-clinic-web/src/main/java/com/cadebe/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/cadebe/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.cadebe.petclinic.bootstrap;
 
 import com.cadebe.petclinic.model.Owner;
+import com.cadebe.petclinic.model.PetType;
 import com.cadebe.petclinic.model.Vet;
 import com.cadebe.petclinic.service.OwnerService;
+import com.cadebe.petclinic.service.PetTypeService;
 import com.cadebe.petclinic.service.VetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,15 +15,26 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
