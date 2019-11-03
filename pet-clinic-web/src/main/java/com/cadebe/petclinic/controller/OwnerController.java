27f43fc6402e738780188544d190b7aafef92645
Carla <carla.debeer.uk@gmail.com>
2019-11-03 14:54:06 +0100

    List all Vets on Vet index page. Closes #9.

--- pet-clinic-web/src/main/java/com/cadebe/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/cadebe/petclinic/controller/OwnerController.java
@@ -1,14 +1,25 @@
 package com.cadebe.petclinic.controller;
 
+import com.cadebe.petclinic.service.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    @Autowired
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
--- pet-clinic-web/src/main/java/com/cadebe/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/cadebe/petclinic/controller/VetController.java
@@ -1,14 +1,25 @@
 package com.cadebe.petclinic.controller;
 
+import com.cadebe.petclinic.service.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
+    private final VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
