8814783db90ec4a202b478fa47551e5cefb38ce6
Carla <carla.debeer.uk@gmail.com>
2019-11-04 13:35:38 +0100

    Implement BaseEntity (JPA). Closes #30.

--- pet-clinic-data/src/main/java/com/cadebe/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/cadebe/petclinic/model/BaseEntity.java
@@ -3,12 +3,19 @@ package com.cadebe.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 @Getter
 @Setter
+@MappedSuperclass // means the class won't be mapped to the db
 public class BaseEntity implements Serializable {
 
     // boxed types can be null whereas primitives cannot. Requirement from JPA to use boxed types.
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 }
