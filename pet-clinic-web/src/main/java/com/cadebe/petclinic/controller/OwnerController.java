7b28e7bbc9a0c0fdd25faa00f02afe5fb6e3e524
Carla <carla.debeer.uk@gmail.com>
2019-11-04 08:54:18 +0100

    Fix Broken Menu Links. Closes #40.

--- pet-clinic-web/src/main/java/com/cadebe/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/cadebe/petclinic/controller/OwnerController.java
@@ -22,4 +22,9 @@ public class OwnerController {
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
+
+    @RequestMapping("/find")
+    public String findOwners() {
+        return "notimplemented";
+    }
 }
--- pet-clinic-web/src/main/java/com/cadebe/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/cadebe/petclinic/controller/VetController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/vets")
+@RequestMapping()
 public class VetController {
 
     private final VetService vetService;
@@ -17,7 +17,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
+    @RequestMapping({"/vets", "/vets/", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model) {
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
