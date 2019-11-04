958a862bc92067fd898d2b90cc0e5118555f8644
Carla <carla.debeer.uk@gmail.com>
2019-11-04 15:46:37 +0100

    Create Spring Data JPA Vet Speciality Service. Closes #39.

---
+++ pet-clinic-data/src/main/java/com/cadebe/petclinic/service/SpecialitySDJpaService.java
@@ -0,0 +1,49 @@
+package com.cadebe.petclinic.service;
+
+import com.cadebe.petclinic.model.Speciality;
+import com.cadebe.petclinic.repository.SpecialityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialitySDJpaService implements SpecialityService {
+
+    private final SpecialityRepository specialityRepository;
+
+    @Autowired
+    public SpecialitySDJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return specialityRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        specialityRepository.deleteById(id);
+    }
+}
