9c73f8bb9ed28ea16e41cd39e91b1e395b098abd
Carla <carla.debeer.uk@gmail.com>
2019-11-04 14:20:55 +0100

    Convert Vets to JPA Entities. Closes #32.

--- pet-clinic-data/src/main/java/com/cadebe/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/cadebe/petclinic/model/Speciality.java
@@ -4,10 +4,17 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 @Getter
 @Setter
 @Builder
+@Entity
+@Table(name = "speciality")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 }
--- pet-clinic-data/src/main/java/com/cadebe/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/cadebe/petclinic/model/Vet.java
@@ -3,13 +3,20 @@ package com.cadebe.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "vet")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
 }
