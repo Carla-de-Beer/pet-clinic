6b1d3cdbc7ceec5881c7375581994a6c7dc1f0c1
Carla <carla.debeer.uk@gmail.com>
2019-11-04 16:58:31 +0100

    Create a Visit Map Service. Closes #42.

---
+++ pet-clinic-data/src/main/java/com/cadebe/petclinic/map/VisitServiceMap.java
@@ -0,0 +1,39 @@
+package com.cadebe.petclinic.map;
+
+import com.cadebe.petclinic.model.Visit;
+import com.cadebe.petclinic.service.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        if (object.getPet() == null || object.getPet().getOwner() == null || object.getPet().getId() == null) {
+            throw new RuntimeException("Invalid visit parameter");
+        }
+        return super.save(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+}
---
+++ pet-clinic-data/src/main/java/com/cadebe/petclinic/service/VisitService.java
@@ -0,0 +1,6 @@
+package com.cadebe.petclinic.service;
+
+import com.cadebe.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
